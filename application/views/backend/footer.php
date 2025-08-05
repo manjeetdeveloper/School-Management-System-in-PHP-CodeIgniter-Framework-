f66c9945198992fa64bd8b52257a023b10a7e0e4
manjeet kumar <manjeetkyp24@gmail.com>
2025-08-05 16:28:25 +0530

    first commit

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+    &copy; <?php echo date('Y')?> | School Management System in PHP using CodeIgniter Web Framework |
+    <strong>Developed by Manjeet Developer, Fullstack LPU</strong>
+</footer>
